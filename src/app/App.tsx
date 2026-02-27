162350fc906b42bd57a54294fda62383679b7842
Vsevolod Kozyr <kvsevolod3011@gmail.com>
2026-02-27 10:30:39 +0200

    Fonts set up

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,7 +3,9 @@ import React from 'react';
 type Props = {};
 
 const App = (props: Props) => {
-  return <div className="bg-red-500">App</div>;
+  return (
+    <div className="bg-red-500 font-integral text-[100px] font-bold italic">App</div>
+  );
 };
 
 export default App;
